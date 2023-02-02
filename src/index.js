2192f828ad322880a9f3c6d53938cf291d61bac3
Michel Felix <36669696+michelfelix@users.noreply.github.com>
2023-02-01 22:58:39 -0300

    Criado e adicionado o primeiro componente

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import './index.css'
 import  ReactDOM from 'react-dom'
 import react from 'react'
+import Primeiro from './components/basicos/Primeiro'
 
-const tag = <strong>Olá React!!</strong>
 
 ReactDOM.render(
     <div>
-        { tag }
+        <Primeiro></Primeiro>
     </div>
     ,
     document.getElementById('root'))
