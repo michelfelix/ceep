4638f9d3150f7b8a3c187ca23cc284e634797651
Michel Felix <36669696+michelfelix@users.noreply.github.com>
2023-02-01 23:21:52 -0300

    Passando as propriedades do componente como parâmetro de função

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import './index.css'
 import  ReactDOM from 'react-dom'
 import react from 'react'
+
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 
 ReactDOM.render(
     <div>
         <Primeiro></Primeiro>
+        <ComParametro
+            titulo="Situação do aluno"
+            subtitulo="Pedro"
+            nota={9.3}
+        />
     </div>
     ,
     document.getElementById('root'))
