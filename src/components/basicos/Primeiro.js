bf0028e159a596482933735d05990ec651223e3f
Michel Felix <36669696+michelfelix@users.noreply.github.com>
2023-02-01 23:02:50 -0300

    Passando informações dinâmicas ao componente

--- src/components/basicos/Primeiro.js
+++ src/components/basicos/Primeiro.js
@@ -1,3 +1,11 @@
-export default function primeiro() {
-    return 'Primeiro componente'
+import React from "react"
+
+export default function Primeiro() {
+    const msg = 'Seja bem vindo(a)!'
+    return (
+        <div>
+            <h2>Primeiro componente</h2>
+            <p>{ msg }</p>
+        </div>
+    )
 }
